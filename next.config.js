3546b614be2eb61a82ac8d874af341e1d5959830
Philipp Rappold <philipprappold@me.com>
2020-04-29 22:40:07 +0200

    Remove comment from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ module.exports = {
     reactRefresh: true,
   },
   env: {
-    // Set the fauna server key in the .env file and make it available
-    // at Build Time.
     FAUNA_SERVER_KEY: process.env.FAUNA_SERVER_KEY,
   },
 }
