40dac6eb841d190394314d0e2270b27ee4ecafbc
Philipp Rappold <philipprappold@me.com>
2020-05-14 13:55:42 +0200

    Add Roboto webfont

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,6 +16,10 @@ const Layout: React.FC<Props> = ({
   <div className={styles.container} {...props}>
     <Head>
       <title>{title}</title>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+        rel="stylesheet"
+      />
     </Head>
     {header}
     <main className={styles.main}>{props.children}</main>
