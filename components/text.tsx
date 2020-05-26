cc49040f2d1f2ec05a543ca3b6daf4f195726b00
Jelle Overbeek <overbeekjelle@gmail.com>
2020-05-26 21:37:11 +0200

    Text styles for mobile and desktop

--- components/text.tsx
+++ components/text.tsx
@@ -5,15 +5,28 @@ import styles from './text.module.css'
 interface Props {
   as?: React.ElementType | string
   meta?: boolean
+  caption?: boolean
+  semibold?: boolean
+  bold?: boolean
 }
 
 const Text: React.FC<Props> = ({
   as: Component = 'span',
   meta = false,
+  caption = false,
+  semibold = false,
+  bold = false,
   children,
   ...props
 }) => {
-  const className = classNames(styles.hstack, meta && styles.meta)
+  const className = classNames(
+    styles.hstack,
+    styles.text,
+    meta && styles.meta,
+    caption && styles.caption,
+    semibold && styles.semibold,
+    bold && styles.semibold
+  )
 
   return (
     <Component className={className} {...props}>
