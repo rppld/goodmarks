99a906d4599fc857d7961a9499a622eabf7fa3c1
Jelle Overbeek <overbeekjelle@gmail.com>
2020-05-27 08:39:23 +0200

    Removes semibold and bold from Text component

--- components/text.tsx
+++ components/text.tsx
@@ -5,25 +5,15 @@ import styles from './text.module.css'
 interface Props {
   as?: React.ElementType | string
   meta?: boolean
-  semibold?: boolean
-  bold?: boolean
 }
 
 export const Text: React.FC<Props> = ({
   as: Component = 'span',
   meta = false,
-  semibold = false,
-  bold = false,
   children,
   ...props
 }) => {
-  const className = classNames(
-    styles.hstack,
-    styles.text,
-    meta && styles.meta,
-    semibold && styles.semibold,
-    bold && styles.semibold
-  )
+  const className = classNames(styles.hstack, styles.text, meta && styles.meta)
 
   return (
     <Component className={className} {...props}>
@@ -35,18 +25,10 @@ export const Text: React.FC<Props> = ({
 export const SmallText: React.FC<Props> = ({
   as: Component = 'span',
   meta = false,
-  semibold = false,
-  bold = false,
   children,
   ...props
 }) => {
-  const className = classNames(
-    styles.hstack,
-    styles.small,
-    meta && styles.meta,
-    semibold && styles.semibold,
-    bold && styles.semibold
-  )
+  const className = classNames(styles.hstack, styles.small, meta && styles.meta)
 
   return (
     <Component className={className} {...props}>
@@ -58,17 +40,13 @@ export const SmallText: React.FC<Props> = ({
 export const CaptionText: React.FC<Props> = ({
   as: Component = 'span',
   meta = false,
-  semibold = false,
-  bold = false,
   children,
   ...props
 }) => {
   const className = classNames(
     styles.hstack,
     styles.caption,
-    meta && styles.meta,
-    semibold && styles.semibold,
-    bold && styles.semibold
+    meta && styles.meta
   )
 
   return (
