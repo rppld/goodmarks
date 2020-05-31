a5f31eebbf078c4bf3e6a8ca8fdc8576f64c4178
Jelle Overbeek <overbeekjelle@gmail.com>
2020-05-31 20:09:41 +0200

    Fixes some merge mistakes

--- components/bookmark.tsx
+++ components/bookmark.tsx
@@ -63,78 +63,54 @@ const Bookmark: React.FC<Props> = ({
     props.onLike(await likeBookmark(bookmark.id))
   }
 
-  function optimisticLikeCount() {
-    if (bookmarkStats.like) {
-      return bookmark.likes - 1
-    }
-    return bookmark.likes + 1
-  }
-
-  function optimisticActiveState() {
-    if (bookmarkStats.like && liking) {
-      return false
-    }
-    if (!bookmarkStats.like && liking) {
-      return true
-    }
-    if (bookmarkStats.like) {
-      return true
-    }
-    return false
-  }
-
   return (
     <div className={styles.container}>
-      <VStack>
-        <HStack alignment="space-between">
-          <HStack>
-            <Avatar
-              src={user.picture && getImageUrl(user.picture, 'avatarLg')}
-            />
-            <div className={styles.userInfo}>
-              <SmallText>@{user.handle}</SmallText>
-              <Caption as="p" meta>
-                <TimeAgo datetime={bookmark.created['@ts']} />
-              </Caption>
-            </div>
-          </HStack>
-          <HStack>
-            <Action
-              active={bookmarkStats.like}
-              leftAdornment={<Heart />}
-              onClick={handleLike}
-              disabled={liking}
-            >
-              {bookmark.likes}
-            </Action>
-            <Action
-              as="span"
-              active={bookmarkStats.comment}
-              leftAdornment={<SpeechBubble />}
-            >
-              {bookmark.comments}
-            </Action>
-          </HStack>
+      <HStack alignment="space-between">
+        <HStack>
+          <Avatar src={user.picture && getImageUrl(user.picture, 'avatarLg')} />
+          <div className={styles.userInfo}>
+            <SmallText>@{user.handle}</SmallText>
+            <Caption as="p" meta>
+              <TimeAgo datetime={bookmark.created['@ts']} />
+            </Caption>
+          </div>
         </HStack>
-        <div className={styles.description}>
-          {bookmark.description && <Text as="p">{bookmark.description}</Text>}
-        </div>
-        <Link href="/b/[id]" as={`/b/${bookmark.id}`}>
-          <a>
-            <Item
-              title={bookmark.title}
-              text={getYear(
-                parseISO(
-                  bookmark.details['first_air_date'] ||
-                    bookmark.details['release_date']
-                )
-              )}
-              image={`https://image.tmdb.org/t/p/w220_and_h330_face/${bookmark.details['poster_path']}`}
-              alt={`Poster for ${bookmark.title}`}
-            />
-          </a>
-        </Link>
-      </VStack>
+        <HStack>
+          <Action
+            active={bookmarkStats.like}
+            leftAdornment={<Heart />}
+            onClick={handleLike}
+            disabled={liking}
+          >
+            {bookmark.likes}
+          </Action>
+          <Action
+            as="span"
+            active={bookmarkStats.comment}
+            leftAdornment={<SpeechBubble />}
+          >
+            {bookmark.comments}
+          </Action>
+        </HStack>
+      </HStack>
+      <div className={styles.description}>
+        {bookmark.description && <Text as="p">{bookmark.description}</Text>}
+      </div>
+      <Link href="/b/[id]" as={`/b/${bookmark.id}`}>
+        <a>
+          <Item
+            title={bookmark.title}
+            text={getYear(
+              parseISO(
+                bookmark.details['first_air_date'] ||
+                  bookmark.details['release_date']
+              )
+            )}
+            image={`https://image.tmdb.org/t/p/w220_and_h330_face/${bookmark.details['poster_path']}`}
+            alt={`Poster for ${bookmark.title}`}
+          />
+        </a>
+      </Link>
     </div>
   )
 }
