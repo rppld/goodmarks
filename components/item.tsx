faeb75fa5104791d3e36b111fed05c05fe67a642
Jelle Overbeek <overbeekjelle@gmail.com>
2020-05-31 15:52:06 +0200

    Rename CaptionText to Caption

--- components/item.tsx
+++ components/item.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './item.module.css'
-import { Text, CaptionText } from './text'
+import { Text, Caption } from './text'
 import { H4 } from './heading'
 import { HStack } from './stack'
 import Image from './image'
@@ -20,7 +20,7 @@ const Item: React.FC<Props> = ({ image, alt, title, text }) => {
         {image && <Image src={image} alt={alt} className={styles.poster} />}
         <div className={styles.meta}>
           <Text as="h4">{title}</Text>
-          <CaptionText meta>{text}</CaptionText>
+          <Caption meta>{text}</Caption>
         </div>
       </HStack>
     </div>
