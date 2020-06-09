18d17d6b16aa40cec9dbe760b227aa588d105a82
Philipp Rappold <philipprappold@me.com>
2020-06-09 14:06:21 +0200

    Statically generate bookmark pages with fallback

--- pages/b/[id].tsx
+++ pages/b/[id].tsx
@@ -3,7 +3,6 @@ import { NextPage } from 'next'
 import useSWR, { mutate } from 'swr'
 import PageTitle from 'components/page-title'
 import Layout from 'components/layout'
-import { useRouter } from 'next/router'
 import Input from 'components/input'
 import Button from 'components/button'
 import Form from 'components/form'
@@ -16,13 +15,19 @@ import useDeleteBookmark from 'utils/use-delete-bookmark'
 import useLikeBookmark from 'utils/use-like-bookmark'
 import useDeleteComment from 'utils/use-delete-comment'
 import useCreateComment from 'utils/use-create-comment'
+import { bookmarkApi } from 'pages/api/bookmarks'
 
-const BookmarkDetail: NextPage = () => {
-  const router = useRouter()
-  const { id } = router.query
+interface Props {
+  initialData: BookmarksData
+  bookmarkId: string
+}
+
+const BookmarkDetail: NextPage<Props> = ({ initialData, bookmarkId }) => {
   const { data, error } = useSWR<BookmarksData>(
-    () => id && `/api/bookmarks?id=${id}`
+    () => bookmarkId && `/api/bookmarks?id=${bookmarkId}`,
+    { initialData }
   )
+  const manuallyRevalidated = React.useRef(false)
   const item = data?.bookmarks?.length > 0 && data.bookmarks[0]
   const { bookmark, bookmarkStats, user, comments } = item
   const { viewer } = useViewer()
@@ -38,9 +43,20 @@ const BookmarkDetail: NextPage = () => {
   const [createComment] = useCreateComment()
   const [deleteComment] = useDeleteComment()
 
+  React.useEffect(() => {
+    // Manually revalidate upon mount in order to fetch the bookmark
+    // stats, e.g. to show whether the viewer likes the bookmark or
+    // not. These stats will always be empty at first, because we
+    // can’t pass the cookie along inside `getStaticProps()`.
+    if (data && viewer && !manuallyRevalidated.current) {
+      mutate(`/api/bookmarks?id=${bookmarkId}`)
+      manuallyRevalidated.current = true
+    }
+  }, [data, bookmarkId, viewer])
+
   async function handleLike() {
-    const { bookmarks } = await likeBookmark(String(id))
-    mutate(`/api/bookmarks?id=${id}`, {
+    const { bookmarks } = await likeBookmark(bookmarkId)
+    mutate(`/api/bookmarks?id=${bookmarkId}`, {
       bookmarks: [
         {
           ...bookmarks[0],
@@ -55,8 +71,8 @@ const BookmarkDetail: NextPage = () => {
 
   async function handleCreateComment(values, { resetForm }) {
     try {
-      const { comment } = await createComment(values.comment, String(id))
-      mutate(`/api/bookmarks?id=${id}`, {
+      const { comment } = await createComment(values.comment, bookmarkId)
+      mutate(`/api/bookmarks?id=${bookmarkId}`, {
         bookmarks: [
           {
             ...data.bookmarks[0],
@@ -84,7 +100,7 @@ const BookmarkDetail: NextPage = () => {
 
   async function handleDeleteComment(commentId) {
     const res = await deleteComment(commentId)
-    mutate(`/api/bookmarks?id=${id}`, {
+    mutate(`/api/bookmarks?id=${bookmarkId}`, {
       bookmarks: [
         {
           ...data.bookmarks[0],
@@ -166,7 +182,7 @@ const BookmarkDetail: NextPage = () => {
           <h2>Danger zone</h2>
           <Button
             variant="danger"
-            onClick={() => deleteBookmark(String(id))}
+            onClick={() => deleteBookmark(bookmarkId)}
             disabled={deleting}
           >
             {deleting ? 'Deleting' : 'Delete bookmark'}
@@ -175,6 +191,19 @@ const BookmarkDetail: NextPage = () => {
       )}
     </Layout>
   )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { id } = params
+  const initialData = await bookmarkApi(id)
+  return { props: { initialData, bookmarkId: id } }
 }
 
 export default BookmarkDetail
