e89dbf6df7c5836197f84eb3a165c34fa4689a0d
Jelle Overbeek <overbeekjelle@gmail.com>
2020-06-16 21:01:49 +0200

    Adds component ListEdge

---
+++ components/list-edge.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './list-edge.module.css'
+import TimeAgo from 'timeago-react'
+import { H4 } from './heading'
+import { Text, SmallText } from './text'
+import Link from 'next/link'
+import { VStack } from './stack'
+
+interface Props {
+  id: string
+  title: string
+  description: string
+  datetime: string
+}
+
+const ListEdge: React.FC<Props> = ({ id, title, description, datetime }) => {
+  return (
+    <Link href={`/list/[id]`} as={`/list/${id}`}>
+      <div className={styles.container}>
+        <VStack>
+          <H4>{title}</H4>
+          <Text>{description}</Text>
+          <SmallText meta>
+            Last updated: <TimeAgo datetime={datetime} />
+          </SmallText>
+        </VStack>
+      </div>
+    </Link>
+  )
+}
+
+export default ListEdge
