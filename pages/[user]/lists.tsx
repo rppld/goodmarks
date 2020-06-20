f1313da205addfe070aa328ca1372ce819a7b450
Philipp Rappold <philipprappold@me.com>
2020-06-20 17:50:08 +0200

    Add pages for lists and creating a new list

---
+++ pages/[user]/lists.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Layout from 'components/layout'
+import { H4 } from 'components/heading'
+import { Text } from 'components/text'
+import PageTitle from 'components/page-title'
+import Button from 'components/button'
+
+const Lists: NextPage = () => {
+  const { query } = useRouter()
+
+  return (
+    <Layout>
+      <PageTitle>
+        <H4 as="h1">Lists</H4>
+        <Text meta>@{query.user}</Text>
+      </PageTitle>
+      <Link href="/lists/new" passHref>
+        <Button as="a" variant="primary">
+          New list
+        </Button>
+      </Link>
+    </Layout>
+  )
+}
+
+export default Lists
