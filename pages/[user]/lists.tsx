741358aaa061d491759246fbfaa5c4d92dd83965
Philipp Rappold <philipprappold@me.com>
2020-06-21 13:02:02 +0200

    Add lists overview

--- pages/[user]/lists.tsx
+++ pages/[user]/lists.tsx
@@ -7,6 +7,7 @@ import { H4 } from 'components/heading'
 import { Text } from 'components/text'
 import PageTitle from 'components/page-title'
 import Button from 'components/button'
+import ListsOverview from 'components/lists-overview'
 
 const Lists: NextPage = () => {
   const { query } = useRouter()
@@ -17,11 +18,14 @@ const Lists: NextPage = () => {
         <H4 as="h1">Lists</H4>
         <Text meta>@{query.user}</Text>
       </PageTitle>
+
       <Link href="/lists/new" passHref>
         <Button as="a" variant="primary">
           New list
         </Button>
       </Link>
+
+      <ListsOverview />
     </Layout>
   )
 }
