0cd40197f272ca643deb5d73ccfa04338550df0c
Philipp Rappold <philipprappold@me.com>
2020-06-22 11:54:57 +0200

    Add Checkbox component

---
+++ components/checkbox.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styles from './checkbox.module.css'
+import inputStyles from './input.module.css'
+import { CustomCheckboxContainer, CustomCheckboxInput } from '@reach/checkbox'
+
+interface Props extends React.ComponentPropsWithoutRef<'input'> {
+  labelText: string
+  help?: React.ReactElement | string
+}
+
+function getCheckStyle(checked) {
+  return {
+    transform: `translate(-50%, -50%) scaleX(${!!checked ? 1 : 0}) scaleY(${
+      checked === true ? 1 : checked === 'mixed' ? 0.4 : 0
+    })`,
+    backgroundColor:
+      checked === true
+        ? 'var(--color-primary)'
+        : checked === 'mixed'
+        ? 'goldenrod'
+        : 'transparent',
+  }
+}
+
+const Checkbox: React.FC<Props> = ({ labelText, help, ...props }) => {
+  const [checkedState, setChecked] = React.useState(props.checked || false)
+  const checked = props.checked != null ? props.checked : checkedState
+
+  return (
+    <div>
+      <label className={styles.container}>
+        <CustomCheckboxContainer
+          checked={props.checked != null ? props.checked : checked}
+          onChange={(event) => {
+            props.onChange(event)
+            setChecked(event.target.checked)
+          }}
+          className={styles.box}
+        >
+          <CustomCheckboxInput {...props} />
+          <span
+            aria-hidden
+            style={getCheckStyle(checked)}
+            className={styles.check}
+          />
+        </CustomCheckboxContainer>
+        <span className={styles.label}>{labelText}</span>
+      </label>
+      {help ? <span className={inputStyles.help}>{help}</span> : null}
+    </div>
+  )
+}
+
+export default Checkbox
