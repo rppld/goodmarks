527f49c4ba8fc382e86ed8e3f6ba5077611307ec
Philipp Rappold <philipprappold@me.com>
2020-06-22 18:16:16 +0200

    Fix Calendar icon

--- components/icon/calendar.tsx
+++ components/icon/calendar.tsx
@@ -3,7 +3,6 @@ import Icon, { IconProps } from './icon'
 
 export const Calendar: React.FC<IconProps> = (props) => (
   <Icon a11yTitle="Calendar" {...props}>
-    <path d="M6 2C5.44772 2 5 2.44772 5 3V4H4C2.89543 4 2 4.89543 2 6V9H22V6C22 4.89543 21.1046 4 20 4H19V3C19 2.44772 18.5523 2 18 2C17.4477 2 17 2.44772 17 3V4H7V3C7 2.44772 6.55228 2 6 2Z" />
-    <path d="M22 11H2V20C2 21.1046 2.89543 22 4 22H20C21.1046 22 22 21.1046 22 20V11Z" />
+    <path d="M6 1a1 1 0 00-1 1v1H4a2 2 0 00-2 2v3h20V5a2 2 0 00-2-2h-1V2a1 1 0 10-2 0v1H7V2a1 1 0 00-1-1zM22 10H2v9a2 2 0 002 2h16a2 2 0 002-2v-9z" />
   </Icon>
 )
