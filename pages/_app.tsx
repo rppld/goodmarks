cbabc46a51508ad990825300b1442db5104ca6a2
Philipp Rappold <philipprappold@me.com>
2020-06-22 18:16:40 +0200

    Add dialog component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import '@reach/combobox/styles.css'
 import '@reach/listbox/styles.css'
 import '@reach/menu-button/styles.css'
 import '@reach/checkbox/styles.css'
+import '@reach/dialog/styles.css'
 import 'lib/styles.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
