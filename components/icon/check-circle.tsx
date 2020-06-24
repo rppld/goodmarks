dba45eb41c88bb9760000c269eb1459da924ced1
Philipp Rappold <philipprappold@me.com>
2020-06-24 14:49:48 +0200

    Use rounded glyphs for icons

--- components/icon/check-circle.tsx
+++ components/icon/check-circle.tsx
@@ -3,6 +3,6 @@ import Icon, { IconProps } from './icon'
 
 export const CheckCircle: React.FC<IconProps> = (props) => (
   <Icon a11yTitle="CheckCircle" {...props}>
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29L5.7 12.7a.996.996 0 111.41-1.41L10 14.17l6.88-6.88a.996.996 0 111.41 1.41l-7.59 7.59a.996.996 0 01-1.41 0z" />
   </Icon>
 )
--- components/icon/error.tsx
+++ components/icon/error.tsx
@@ -3,6 +3,6 @@ import Icon, { IconProps } from './icon'
 
 export const Error: React.FC<IconProps> = (props) => (
   <Icon a11yTitle="Error" {...props}>
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 11c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z" />
   </Icon>
 )
