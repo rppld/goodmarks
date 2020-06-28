9181a7aab8012803f3ef15715a2b09bdcd5775dd
Jelle Overbeek <overbeekjelle@gmail.com>
2020-06-28 14:02:40 +0200

    Simplifies category tiles to a component

--- components/category-selection.tsx
+++ components/category-selection.tsx
@@ -6,6 +6,37 @@ import styles from './category-selection.module.css'
 import { Text } from './text'
 import { Movie, Link as LinkIcon, TV } from './icon'
 
+interface props {
+  linkHref: string
+  linkAs: string
+  label: string
+  icon: any
+  className: any
+}
+
+const CategoryTile: React.FC<props> = ({
+  linkHref,
+  linkAs,
+  label,
+  icon,
+  className,
+}) => {
+  return (
+    <li>
+      <Link href={linkHref} as={linkAs}>
+        <a>
+          <div className={classNames(styles.category, className)}>
+            {icon}
+            <div className={styles.label}>
+              <Text>{label}</Text>
+            </div>
+          </div>
+        </a>
+      </Link>
+    </li>
+  )
+}
+
 const CategorySelection: React.FC = () => {
   const router = useRouter()
   const queryString = router && router.asPath.split('?')[1]
@@ -13,42 +44,29 @@ const CategorySelection: React.FC = () => {
 
   return (
     <ul className={styles.list}>
-      <li>
-        <Link href="/b/new/[category]" as={`/b/new/movie${query}`}>
-          <a>
-            <div className={classNames(styles.category, styles.movies)}>
-              <Movie size="md" />
-              <div className={styles.label}>
-                <Text>Movie</Text>
-              </div>
-            </div>
-          </a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/b/new/[category]" as={`/b/new/tv-show${query}`}>
-          <a>
-            <div className={classNames(styles.category, styles['tv-shows'])}>
-              <TV size="md" />
-              <div className={styles.label}>
-                <Text>TV show</Text>
-              </div>
-            </div>
-          </a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/b/new/[category]" as={`/b/new/link${query}`}>
-          <a>
-            <div className={classNames(styles.category, styles.links)}>
-              <LinkIcon size="md" />
-              <div className={styles.label}>
-                <Text>Link</Text>
-              </div>
-            </div>
-          </a>
-        </Link>
-      </li>
+      <CategoryTile
+        linkHref="/b/new/[category]"
+        linkAs={`/b/new/movie${query}`}
+        label="Movie"
+        icon={<Movie size="md" />}
+        className={styles.movies}
+      />
+
+      <CategoryTile
+        linkHref="/b/new/[category]"
+        linkAs={`/b/new/tv-show${query}`}
+        label="TV show"
+        icon={<TV size="md" />}
+        className={styles['tv-shows']}
+      />
+
+      <CategoryTile
+        linkHref="/b/new/[category]"
+        linkAs={`/b/new/link${query}`}
+        label="Link"
+        icon={<LinkIcon size="md" />}
+        className={styles.links}
+      />
     </ul>
   )
 }
