7906be9aba18921e0fcf9d5ab7f7cc94cb480c13
Jelle Overbeek <overbeekjelle@gmail.com>
2020-06-28 15:29:56 +0200

    Adds hover states to authorinfo links

--- components/author-info.tsx
+++ components/author-info.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 import styles from './author-info.module.css'
 import Link from 'next/link'
 import Avatar from './avatar'
@@ -18,14 +19,21 @@ const AuthorInfo: React.FC<Props> = ({ user, createdAt, subtitle }) => {
   return (
     <HStack alignment="leading">
       <Link href="/[user]" as={`/${user.handle}`}>
-        <a className="action">
-          <Avatar src={user.picture && getImageUrl(user.picture, 'avatarLg')} />
+        <a>
+          <div className={classNames('action', styles.action)}>
+            <Avatar
+              src={user.picture && getImageUrl(user.picture, 'avatarLg')}
+            />
+          </div>
         </a>
       </Link>
+
       <div className={styles.authorInfo}>
         <Link href="/[user]" as={`/${user.handle}`}>
-          <a className="action">
-            <SmallText>@{user.handle}</SmallText>
+          <a>
+            <div className={classNames('action', styles.action)}>
+              <SmallText>@{user.handle}</SmallText>
+            </div>
           </a>
         </Link>
         <Caption as="p" meta>
