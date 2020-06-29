faeef825b97854105065e8becb7c5a0d909303a9
Philipp Rappold <philipprappold@me.com>
2020-06-29 11:36:24 +0200

    Style infinite scroll trigger and allow children

--- components/infinite-scroll-trigger.tsx
+++ components/infinite-scroll-trigger.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import useIntersect from 'utils/use-intersect'
 import usePrevious from 'utils/use-previous'
+import styles from './infinite-scroll-trigger.module.css'
 
 interface Props {
   onIntersect: () => void
   disabled: boolean
 }
 
-const InfiniteScrollTrigger: React.FC<Props> = (props) => {
+const InfiniteScrollTrigger: React.FC<Props> = ({ children, ...props }) => {
   const ref = React.useRef()
   const visible = useIntersect(ref)
   const previouslyVisible = usePrevious(visible)
@@ -18,7 +19,11 @@ const InfiniteScrollTrigger: React.FC<Props> = (props) => {
     }
   })
 
-  return <span ref={ref} />
+  return (
+    <div ref={ref} className={styles.container}>
+      {children}
+    </div>
+  )
 }
 
 InfiniteScrollTrigger.defaultProps = {
