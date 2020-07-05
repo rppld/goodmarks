7b6a5da6f0af5526de37c52df51eab161c7eaffe
Philipp Rappold <philipprappold@me.com>
2020-07-05 17:46:18 +0200

    Restructure list overview

--- components/lists-overview.tsx
+++ components/lists-overview.tsx
@@ -13,33 +13,35 @@ interface Props {
 const ListsOverview: React.FC<Props> = ({ handle }) => {
   const { data, error } = useSWR(`/api/lists?handle=${handle}`)
 
-  if (data?.edges?.length === 0) {
-    return (
-      <div className={styles['empty-state']}>
-        <SmallText meta>{handle} doesn't have any lists yet</SmallText>
-      </div>
-    )
-  } else if (data) {
-    return (
-      <VStack spacing="md">
-        {data?.edges?.map(({ list }) => (
-          <div key={list.id}>
-            <Link href="/[user]/lists/[id]" as={`/${handle}/lists/${list.id}`}>
-              <a>
-                <ListNode list={list} />
-              </a>
-            </Link>
-          </div>
-        ))}
-      </VStack>
-    )
-  } else {
+  if (!data) {
     return (
       <div className={styles['empty-state']}>
         <SmallText meta>Loading...</SmallText>
       </div>
     )
   }
+
+  if (data.edges.length === 0) {
+    return (
+      <div className={styles['empty-state']}>
+        <SmallText meta>@{handle} doesn't have any lists yet</SmallText>
+      </div>
+    )
+  }
+
+  return (
+    <VStack spacing="md">
+      {data?.edges?.map(({ list }) => (
+        <div key={list.id}>
+          <Link href="/[user]/lists/[id]" as={`/${handle}/lists/${list.id}`}>
+            <a>
+              <ListNode list={list} />
+            </a>
+          </Link>
+        </div>
+      ))}
+    </VStack>
+  )
 }
 
 export default ListsOverview
