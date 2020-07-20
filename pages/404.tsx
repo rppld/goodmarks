081b3a6482d1ec0a30e652cbc19991045fc11ede
Jelle Overbeek <overbeekjelle@gmail.com>
2020-07-20 20:20:04 +0200

    Updates copy of 404 page, removes URL to Home

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,19 +4,17 @@ import Layout from 'components/layout'
 import { H4 } from 'components/heading'
 import { Text } from 'components/text'
 import PageTitle from 'components/page-title'
-import Link from 'next/link'
 
 const PageNotFound: NextPage = () => {
   return (
     <Layout>
       <PageTitle>
-        <H4 as="h1">404 - The page doesn't exist</H4>
+        <H4 as="h1">404</H4>
         <Text meta>
-          The page you're looking for could be deleted or moved. Another common
-          cause for this error is typos in the URL.
+          The page you’re looking for could have been deleted or moved. Or do
+          you maybe have a typo in the URL?
         </Text>
       </PageTitle>
-      <Link href="/">Return to home</Link>
     </Layout>
   )
 }
