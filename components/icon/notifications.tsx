8cb1bc530dfb6e4352a91508ae6b0d2dbd712f79
Philipp Rappold <philipprappold@me.com>
2020-07-28 22:23:02 +0200

    Add notifications icon

---
+++ components/icon/notifications.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Icon, { IconProps } from './icon'
+
+export const Notifications: React.FC<IconProps> = (props) => (
+  <Icon a11yTitle="Notifications" {...props}>
+    <path d="M12 22c1.1 0 2-.9 2-2h-4a2 2 0 002 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" />
+  </Icon>
+)
