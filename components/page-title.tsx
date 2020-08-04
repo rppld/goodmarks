9f9bc017d593db90194b110197feb24bbc29bb29
Philipp Rappold <philipprappold@me.com>
2020-08-04 19:32:33 +0200

    Add PageTitle adornment

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -5,14 +5,21 @@ import styles from './page-title.module.css'
 interface Props {
   as?: React.ElementType | string
   center?: boolean
+  adornment?: React.ReactElement
 }
 
-const PageTitle: React.FC<Props> = ({ children, center = false, ...props }) => {
+const PageTitle: React.FC<Props> = ({
+  children,
+  adornment,
+  center = false,
+  ...props
+}) => {
   const className = clsx(styles.container, center && styles.center)
 
   return (
     <header className={className} {...props}>
       {children}
+      {adornment ? <div className={styles.adornment}>{adornment}</div> : null}
     </header>
   )
 }
