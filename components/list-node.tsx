d08e82a79a6f64c96abc0449b3e546da0c3a8386
Jelle Overbeek <overbeekjelle@gmail.com>
2020-08-04 22:00:04 +0200

    Small tweaks to list node to improve hierarchy, Copy tweaks for notification node

--- components/list-node.tsx
+++ components/list-node.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './list-node.module.css'
-import { H4 } from './heading'
+import { H5 } from './heading'
 import { Text, SmallText } from './text'
 import { VStack } from './stack'
 import { List } from 'lib/types'
@@ -14,7 +14,7 @@ const ListNode: React.FC<Props> = ({ list }) => {
   return (
     <div className={styles.container}>
       <VStack>
-        <H4>{list.name}</H4>
+        <H5>{list.name}</H5>
         <Text as="p">{list.description}</Text>
         <SmallText meta as="p">
           Last updated: <TimeAgo datetime={new Date(list.ts / 1000)} />
--- components/notification-node.tsx
+++ components/notification-node.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import styles from './list-node.module.css'
-import { H4 } from './heading'
-import { Text } from './text'
+import { H5 } from './heading'
+import { SmallText } from './text'
 import { VStack } from './stack'
 import { Notification } from 'lib/types'
+import TimeAgo from 'timeago-react'
 
 interface Props {
   notification: Notification
 }
 
 const ListNode: React.FC<Props> = ({ notification, ...props }) => {
+  const getNotificationTitle = function (): String {
+    let title = 'Unknown notification'
+    let sender = 'Someone'
+
+    if (notification.type === 'NEW_LIKE') {
+      title = `${sender} liked your bookmark.`
+    } else if (notification.type === 'NEW_COMMENT') {
+      title = `${sender} commented on your bookmark.`
+    }
+
+    return title
+  }
+
   return (
     <div className={styles.container} {...props}>
       <VStack>
-        <H4>{notification.type}</H4>
-        <Text as="p">Description</Text>
+        <H5>{getNotificationTitle()}</H5>
+        <SmallText as="p" meta>
+          <TimeAgo datetime={notification.created['@ts']} />
+        </SmallText>
       </VStack>
     </div>
   )
