bd600c1ffe5a1b28a0ac15159c170db71cae5db2
Philipp Rappold <philipprappold@me.com>
2020-08-04 19:32:33 +0200

    Add PageTitle adornment

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { NextPage } from 'next'
 import Layout from 'components/layout'
 import { H4 } from 'components/heading'
+import { Notifications as NotificationsIcon } from 'components/icon'
 import PageTitle from 'components/page-title'
 import NotificationsFeed from 'components/notifications-feed'
 
 const Notifications: NextPage = () => {
   return (
     <Layout>
-      <PageTitle>
+      <PageTitle adornment={<NotificationsIcon />}>
         <H4 as="h1">Notifications</H4>
       </PageTitle>
       <NotificationsFeed />
