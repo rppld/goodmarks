28da9368b60fcd3566790b625bde5df6673f4cb5
Jelle Overbeek <overbeekjelle@gmail.com>
2020-08-04 19:59:48 +0200

    Adds notification button, tweaks small button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,26 @@ import { H4 } from 'components/heading'
 import BookmarksTabs from 'components/bookmarks-tabs'
 import BookmarksFeed from 'components/bookmarks-feed'
 import JoinGoodmarks from 'components/join-goodmarks'
+import { Notifications } from 'components/icon'
+import Button from 'components/button'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <PageTitle>
+      <PageTitle
+        adornment={
+          <Link href="/notifications" passHref>
+            <Button
+              as="a"
+              size="sm"
+              leftAdornment={<Notifications size="xs" />}
+            >
+              Notifications
+            </Button>
+          </Link>
+        }
+      >
         <H4 as="h1">Bookmarks</H4>
       </PageTitle>
       <JoinGoodmarks />
