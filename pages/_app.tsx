77f41a46465a1ec3a28a6ca070561138dee98c8e
Philipp Rappold <philipp.rappold@mollie.com>
2020-09-20 13:52:52 +0200

    Improve password-reset UX

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { SWRConfig } from 'swr'
 import fetch from 'lib/fetch'
 import { AppProps } from 'next/app'
+import { ToastContainer, Slide } from 'react-toastify'
 import { ViewerProvider } from 'components/viewer-context'
+import 'react-toastify/dist/ReactToastify.css'
 import '@reach/combobox/styles.css'
 import '@reach/listbox/styles.css'
 import '@reach/menu-button/styles.css'
@@ -16,6 +18,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <ViewerProvider>
         <Component {...pageProps} />
       </ViewerProvider>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={true}
+        transition={Slide}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+      />
     </SWRConfig>
   )
 }
