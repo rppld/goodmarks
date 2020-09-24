b222f36c09dd3426bd2a1a8c72bb6a5c1a952dae
Jelle Overbeek <overbeekjelle@gmail.com>
2020-09-24 22:05:33 +0200

    Add Ackee tracker

---
+++ components/ackee-tracker.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import * as ackeeTracker from 'ackee-tracker'
+
+const AckeeTracker: React.FC = () => {
+  let instance = null
+
+  if (typeof window !== 'undefined') {
+    instance = ackeeTracker
+      .create(
+        {
+          server: 'https://analytics.goodmarks.app',
+          domainId: '939c31b9-e4a6-4992-a47f-52190cbf195b',
+        },
+        {
+          ignoreLocalhost: true,
+          detailed: true,
+        }
+      )
+      .record()
+  }
+
+  return null
+}
+
+export default AckeeTracker
