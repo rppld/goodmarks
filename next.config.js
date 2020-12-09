d6e94787087fa4439484806ed7bf72bd9348d158
Philipp Rappold <philipp.rappold@mollie.com>
2020-12-09 20:06:08 +0100

    Fix pagination for real

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ['dbb78kjxr16nd.cloudfront.net', 'image.tmdb.org'],
+    domains: [
+      'dbb78kjxr16nd.cloudfront.net',
+      'image.tmdb.org',
+      'lh5.googleusercontent.com',
+    ],
   },
 }
